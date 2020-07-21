c23cccc3c5d0250fdf7cb2c3fbf0300fcef97326
Wolfhard Prell <wolfhard.prell@gmail.com>
2020-07-21 22:03:37 +0200

    simulator generator

--- src/app.js
+++ src/app.js
@@ -1,64 +1,25 @@
-import Vector from './models/vector.js';
-import Planet from './models/planet.js';
-import SolarSystem from './models/solar-system.js';
+import simulate from './simulation.js';
 
-window.memento = [];
-const c = document.getElementById('solar-system-canvas');
-const ctx = c.getContext('2d');
-const origin = new Vector(c.width / 2, c.height / 2, 0.0);
-const startMass = 1.0;
-const startVelocity = new Vector(0.1, 0.1, 0.1);
-const startAcceleration = new Vector(0.1, 0.1, 0.0);
-const planets = [
-    new Planet(startMass, origin, startVelocity, startAcceleration, 'black'),
-    new Planet(startMass, new Vector(10.0, 10.0, 0.0), startVelocity, startAcceleration, 'red'),
-    new Planet(startMass, new Vector(190.0, 190.0, 0.0), startVelocity, startAcceleration, 'green'),
-];
-const solarSystem = new SolarSystem(planets, origin);
-save(solarSystem, 0);
+function startSimulation() {
+    const multiplier = 1;
+    var n = parseInt(document.getElementById('n-input').value) * multiplier;
+    console.log('simulating', n, 'planets');
 
-function simulate() {
-    const startTime = new Date().getTime();
-    solarSystem.tick();
-    const endTime = new Date().getTime();
-    const diff = endTime - startTime;
-    console.log("time taken ", diff, "ms");
-    save(solarSystem, diff);
-    updateUI(solarSystem);
+    const result = [];
+
+    for (var i = 0; i < 10; ++i ) {
+        console.log(i);
+        n *= 10;
+        const start = performance.now();
+        simulate(n, n);
+        const end = performance.now();
+        const timeTaken = end - start;
+        console.log(n, 'runs with', n, 'planets took', timeTaken, 'ms');
+        result.push({n: n, runs: n, timeTaken: timeTaken});
+    }
+
+    console.log(JSON.stringify(result));
+    console.table(result);
 }
 
-function save(solarSystem, time) {
-    window.memento.push({});
-    const lastIndex = window.memento.length - 1;
-    const lastElement = window.memento[lastIndex];
-    Object.assign(lastElement, solarSystem);
-    lastElement.time = time;
-}
-
-function updateUI(solarSystem) {
-    // console.log(memento);
-
-    // ctx.save();
-    // ctx.translate(15, 0);
-    ctx.clearRect(0, 0, c.width, c.height);
-    solarSystem.planets.forEach(planet => {
-        ctx.beginPath();
-        const x = planet.position.x;
-        const y = planet.position.y;
-        ctx.arc(x, y, 5, 0, 2 * Math.PI);
-        ctx.fillStyle = planet.color;
-        ctx.fill();
-    });
-}
-
-function init() {
-    updateUI(memento[0]);
-}
-
-function reset() {
-    window.memento = [];
-}
-
-window.init = init;
-window.reset = reset;
-window.simulate = simulate;
+window.startSimulation = startSimulation;
---
+++ src/simulation.js
@@ -0,0 +1,26 @@
+import Vector from './models/vector.js';
+import Planet from './models/planet.js';
+import SolarSystem from './models/solar-system.js';
+
+
+const origin = new Vector(1.0, 1.0, 1.0);
+
+export default function simulate(n, runs) {
+    const planets = generatePlanets(n);
+    const solarSystem = new SolarSystem(planets, origin);
+    for (var i = 0; i < runs; ++i) {
+        solarSystem.tick();
+    }
+}
+
+function generatePlanets(n) {
+    const planets = [];
+    for (var i; i < n; ++i) {
+        const startMass = i;
+        const startVelocity = new Vector(i, i, i);
+        const startAcceleration = new Vector(i, i, i);
+        const planet = new Planet(startMass, origin, startVelocity, startAcceleration);
+        planets.push(planet);
+    }
+    return planets;
+}
